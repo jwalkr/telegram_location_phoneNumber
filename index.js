383d1767616da727b88533b1b94b9d17d68d9844
Sakhiwo Mkwebula <sakhiwo@thedigitalacademy.co.za>
2018-10-13 12:23:44 +0200

    Telegraf

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
-const telegramBot = require('node-telegram-bot-api')
-const ogs = require('open-graph-scraper')
-const firebase = require('firebase')
+const telegraf = require('telegraf')
 
+const bot = new telegraf('638803907:AAEkXe2bJhZ-mEdVkpoMNJ1NgMBkYUcYH4A')
 
-const token = '638803907:AAEkXe2bJhZ-mEdVkpoMNJ1NgMBkYUcYH4A'
-const bot = new telegramBot(token, {polling: true})
-
-
-bot.on('message' , (msg) => {
- let option = {
-     "parse_mode" : "Markdown",
-     "reply_markup": {
-         "one_time_keyboard": true,
-         "keyboard": [[{
-             text: "My phone number" , 
-             request_contact: true
-         }],["Cancel"]]
-     }
- }
-
- bot.sendMessage(msg.chat.id , "How can we contact you ?" , option)
- .then(()=>{
-    bot.once("contact" , (msg) => {
-        let option = {
-             "parse_mode": "Markdown" ,
-             "reply_markup": {
-                 "one_time_keyboard": [[{
-                     text: "My location",
-                     request_location: true
-                 }], ["Cancel"]]
-            }
-        }
-        bot.sendMessage(msg.chat.id , util.format('Thank you %s with phone%s! And where are you ?' , msg.contact.first_name , msg.contact.phone_number), option)
-        .then(() => {
-            bot.once("location" , (msg) => {
-                bot.sendMessage(msg.chat.id , "We will deliver your order to " + [msg.location.longitude , msg.location.latitude].join(";"))
-            })
-        })
-    })
-    
- })
-
-})
+bot.start((ctx) => ctx.reply('welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker' , (ctx) => ctx.reply('👍'))
+bot.hears('hi' , (ctx) => ctx.reply('Hey there'))
+bot.hears(/buy/i , (ctx) => ctx.reply('Buy-buy'))
+bot.startPolling()
